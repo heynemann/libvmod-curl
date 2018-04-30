bc030a7f353f15a8cf2847dee734c1bb644a1e8d
Bernardo Heynemann <heynemann@gmail.com>
2018-04-30 18:53:20 -0300

    10000 conns

--- src/vmod_curl.c
+++ src/vmod_curl.c
@@ -44,9 +44,9 @@ enum debug_flags {
 #undef DBG
 };
 
-const int MAX_HANDLES = 5000;
-static CURL *curl_handles[5000];
-static struct lock locks[5000];
+const int MAX_HANDLES = 10000;
+static CURL *curl_handles[10000];
+static struct lock locks[10000];
 static pthread_mutex_t init_mutex;
 
 struct vmod_curl {
